d83658151a965c0b3c9d988953204e45c04005b4
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-10-02 23:35:58 -0300

    Add DbContext class and import it as a service

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+}); // Add DbContext as a service
 
 var app = builder.Build();
 
