443a91715a1499680df166f28bf40f285c85a2a7
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-10-05 12:41:33 -0300

    Add local certificates to run site as http in development

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddCors(); //add CORS
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); //add CORS
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); //add CORS
 
 app.MapControllers();
 
