d3d40ca613794d7e40813432d38bb1e696bf256b
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-10-04 22:42:22 -0300

    Add a new API Controller and create first gets

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers(){
+        var users = _context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("{id}")] // api/users/2
+    public ActionResult<AppUser> GetUser(int id){
+        return _context.Users.Find(id);
+    }
+}
