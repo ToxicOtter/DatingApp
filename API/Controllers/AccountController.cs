92fb22af60e1b0b6c5336ce9db44492b0ebf0db7
ToxicOtter <luan.lima.lopes16@gmail.com>
2023-10-06 13:03:28 -0300

    Create Account Controller and account register endpoint. Also, create a Base Api Controller to be extended by all controllers.

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public AccountController (DataContext context){
+            _context = context;
+        }
+
+        [HttpPost("register")] // api/account/register
+        public async Task<ActionResult<AppUser>> Register (string username, string password)
+        {
+            using var hmac = new HMACSHA512(); // we use "using" here because the class have a dispose method, so when we are no using it anymore, the class is finished
+            var user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }  
+}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      this.http.get('http://localhost:5001/api/users').subscribe({
+      this.http.get('https://localhost:5001/api/users').subscribe({
         next: response => this.users = response,
         error: error => console.log(error),
         complete: () => console.log('Request has completed')
