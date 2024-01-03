9e381b9ded9a75287edb92c55bad5818fddc6011
ToxicOtter <luan.lima.lopes16@gmail.com>
2024-01-02 22:35:47 -0300

    Add validation to API fields

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class RegisterDTO
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
 
     }
