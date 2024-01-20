6931b9173a420ab9117742279c97745bd1405a64
ToxicOtter <luan.lima.lopes16@gmail.com>
2024-01-19 22:02:30 -0300

    Add angular template forms and Create account service to handle login requests

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+    model: any = {};
+    loggedIn = false;
 
+    constructor(private accountService: AccountService){};
+
+    ngOnInit(): void{
+
+    }
+
+    login() {
+      this.accountService.login(this.model)
+        .subscribe({
+          next: response => {
+            console.log(response)
+          },
+          error: error => console.log(error)
+        })
+    }
 }
