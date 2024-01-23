458bb1ed13fa5df34fc534b790c2c3ee2d4bacac
ToxicOtter <luan.lima.lopes16@gmail.com>
2024-01-23 20:49:38 -0300

    Add persisting login functionality using observables

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +12,27 @@ export class AppComponent implements OnInit {
   title: string = 'Dating App';
   users: any;
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private accountService: AccountService){
 
   }
 
   ngOnInit(): void {
-      this.http.get('https://localhost:5001/api/users').subscribe({
+      this.getUsers();
+      this.setCurrentUser();
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe({
         next: response => this.users = response,
         error: error => console.log(error),
         complete: () => console.log('Request has completed')
       });
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -13,7 +13,15 @@ export class NavComponent implements OnInit {
     constructor(private accountService: AccountService){};
 
     ngOnInit(): void{
+      this.getCurrentUser();
+    }
 
+    getCurrentUser(){
+      this.accountService.currentUser$
+        .subscribe({
+          next: user => this.loggedIn = !!user,
+          error: error => console.log(error)
+        })
     }
 
     login() {
@@ -28,6 +36,7 @@ export class NavComponent implements OnInit {
     }
 
     logout() {
+      this.accountService.logout();
       this.loggedIn = false;
     }
 }
