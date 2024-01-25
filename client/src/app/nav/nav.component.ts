36dd1669ffb1268beb596ba895595171c7d78260
ToxicOtter <luan.lima.lopes16@gmail.com>
2024-01-25 17:15:21 -0300

    Add toastr to show error notifications at login and register

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -12,7 +13,8 @@ import { Router } from '@angular/router';
 export class NavComponent implements OnInit {
     model: any = {};
 
-    constructor(public accountService: AccountService, private router: Router){};
+    constructor(public accountService: AccountService, private router: Router,
+      private toastr: ToastrService){};
 
     ngOnInit(): void{
     }
@@ -21,7 +23,7 @@ export class NavComponent implements OnInit {
       this.accountService.login(this.model)
         .subscribe({
           next: () => this.router.navigateByUrl('/members'),
-          error: error => console.log(error)
+          error: error => this.toastr.error(error.error)
         })
     }
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,15 +11,15 @@ export class RegisterComponent {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService){}
+  constructor(private accountService: AccountService, private toastr: ToastrService){}
 
   register(){
     this.accountService.register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => console.log(error)
+      next: () => this.cancel(),
+      error: error => {
+        this.toastr.error(error.error),
+        console.log(error)
+      }
     })
   }
 
