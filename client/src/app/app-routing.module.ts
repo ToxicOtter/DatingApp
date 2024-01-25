bd597f7a3dd70725ca966cff62e00945911be9d4
ToxicOtter <luan.lima.lopes16@gmail.com>
2024-01-25 17:31:37 -0300

    Block routes to users and ocult if not logged in

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,10 +9,16 @@ import { authGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberListComponent, canActivate: [authGuard]},
-  {path: 'members/:id', component: MemberDetailComponent},
-  {path: 'lists', component: ListsComponent},
-  {path: 'messages', component: MessagesComponent},
+  {path: '', 
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      {path: 'members', component: MemberListComponent},
+      {path: 'members/:id', component: MemberDetailComponent},
+      {path: 'lists', component: ListsComponent},
+      {path: 'messages', component: MessagesComponent}
+    ]
+  },
   {path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
 
