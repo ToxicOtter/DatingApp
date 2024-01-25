c3a86c96dc5beefbbf3661e46d1527944e5294b3
ToxicOtter <luan.lima.lopes16@gmail.com>
2024-01-25 16:50:43 -0300

    Add routing by nav links and by code

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
     model: any = {};
 
-    constructor(public accountService: AccountService){};
+    constructor(public accountService: AccountService, private router: Router){};
 
     ngOnInit(): void{
     }
@@ -19,14 +20,13 @@ export class NavComponent implements OnInit {
     login() {
       this.accountService.login(this.model)
         .subscribe({
-          next: response => {
-            console.log(response);
-          },
+          next: () => this.router.navigateByUrl('/members'),
           error: error => console.log(error)
         })
     }
 
     logout() {
       this.accountService.logout();
+      this.router.navigateByUrl('/');
     }
 }
