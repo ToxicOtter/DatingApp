1bc4c4ee2ce48a9737e2bf3a8d0ba3ded2bd2520
ToxicOtter <luan.lima.lopes16@gmail.com>
2024-01-27 21:17:31 -0300

    Extend user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,7 @@
-﻿namespace API;
+﻿using API.Entities;
+using API.Extensions;
+
+namespace API;
 
 public class AppUser
 {
@@ -6,4 +9,19 @@ public class AppUser
     public string UserName {get ; set;}
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string KnownAs { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public string Gender { get; set; }
+    public string LookingFor { get; set; }
+    public string Interests { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public List<Photo> Photos { get; set; } = new();
+
+    public int GetAge()
+    {
+        return DateOfBirth.CalculateAge();
+    }
 }
