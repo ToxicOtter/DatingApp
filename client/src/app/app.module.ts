63f62287f26017137fb3b8156f4d89cacbea164d
ToxicOtter <luan.lima.lopes16@gmail.com>
2024-02-08 17:35:30 -0300

    Add user cards

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MessagesComponent,
     TestErrorComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
